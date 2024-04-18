105fc96f1018f2886f6222402e58027433ea8c4a
EmmanuelAdu <christorenzo20@gmail.com>
2024-04-18 12:19:12 +0000

    Returns sum of list of floating-point numbers

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Complex types - list of floats
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Returns the sum of a list of float numbers
+    '''
+    return float(sum(input_list))
