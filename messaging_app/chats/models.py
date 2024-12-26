0f8c7d2693aa384247a50d676f359bf5345d5179
EmmanuelAdu <christorenzo20@gmail.com>
2024-12-26 10:07:38 +0000

    Project Models created

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -8,6 +8,9 @@ class User(AbstractUser):
     """ User Fields
     """
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=128)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     ROLE_CHOICES = [
