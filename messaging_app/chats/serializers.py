fcc755854d122ddd660a7c6ea980ef018f09bbf9
EmmanuelAdu <christorenzo20@gmail.com>
2024-12-28 10:28:23 +0000

    Created serializers for class objects

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,9 +3,16 @@ from .models import User, Conversation, Message
 
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['user_id', 'first_name', 'last_name', 'password', 'email', 'phone_number', 'role', 'created_at']
+        fields = ['user_id', 'first_name', 'last_name', 'password', 'email', 'phone_number', 'role', 'created_at', 'full_name']
+    
+    def get_full_name(self, obj):
+        """Combine first name and last name to full name"""
+        return f"{obj.first_name} {obj.last_name}"
+
 
 
 class MessageSerializer(serializers.ModelSerializer):
@@ -13,10 +20,16 @@ class MessageSerializer(serializers.ModelSerializer):
     sender_id = serializers.PrimaryKeyRelatedField(
         queryset=User.objects.all(), source='sender', write_only=True
     )
+    message_preview = serializers.CharField(source='message_body', read_only=True)
 
     class Meta:
         model = Message
-        fields = ['message_id', 'sender', 'conversation', 'message_body', 'sent_at']
+        fields = ['message_id', 'sender', 'conversation', 'message_preview', 'message_body', 'sent_at']
+    
+    def validate_message_body(self, value):
+        if len(value) < 5:
+            raise serializers.ValidationError("Message body must be atleast 5 characters long")
+        return value
 
 
 class ConversationSerializer(serializers.ModelSerializer):
@@ -25,7 +38,15 @@ class ConversationSerializer(serializers.ModelSerializer):
         queryset=User.objects.all(), many=True, source='participants', write_only=True
     )
     messages = MessageSerializer(many=True, read_only=True)
+    latest_message = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
-        fields = ['conversation_id', 'participants_id', 'created_at']
+        fields = ['conversation_id', 'participants_id', 'latest_message', 'messages', 'created_at']
+    
+    def get_latest_message(self, obj):
+        """Retrieve the latest message in the conversation"""
+        latest_message = obj.messages.order_by('-sent_at').first()
+        if latest_message:
+            return MessageSerializer(latest_message).data
+        return None
